1b938f89399bfaf547225c35082ae280d9bd979b
luolibing <397911353@qq.com>
2017-06-22 10:00:58 +0800

    Revert "spring boot activiti"
    This reverts commit 649f8baf4e7cf15c5c70e2b02592e33bf58b7fce.

--- spring-boot-sample/spring-boot-activiti/src/main/java/cn/tim/activiti/HireProcessRestController.java
+++ spring-boot-sample/spring-boot-activiti/src/main/java/cn/tim/activiti/HireProcessRestController.java
@@ -1,15 +1,12 @@
 package cn.tim.activiti;
 
-import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.RuntimeService;
-import org.activiti.engine.repository.ProcessDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,15 +18,10 @@ public class HireProcessRestController {
     @Autowired
     private ApplicantRepository applicantRepository;
 
-    @Autowired
-    private ProcessEngine processEngine;
-
     @ResponseStatus(value = HttpStatus.OK)
     @RequestMapping(value = "/start-hire-process", method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public void startHireProcess(@RequestBody Map<String, String> data) {
-        // 查看当前正在执行的流程
-        // Context.getExecutionContext().getProcessDefinition().getId();
 
         Applicant applicant = new Applicant(data.get("name"), data.get("email"), data.get("phoneNumber"));
         applicantRepository.save(applicant);
@@ -37,11 +29,6 @@ public class HireProcessRestController {
         Map<String, Object> vars = Collections.<String, Object>singletonMap("applicant", applicant);
         // startProcess开启任务,传递参数
         runtimeService.startProcessInstanceByKey("test", vars);
-
-        List<ProcessDefinition> list = processEngine.getRepositoryService().createProcessDefinitionQuery().list();
-        for(ProcessDefinition processDefinition : list) {
-            System.out.println("processDefinition = " + processDefinition);
-        }
     }
 
 }
