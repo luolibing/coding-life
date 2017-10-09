3b155d9b00a39ea9126d3638669ef2e1926617a6
luolibing <397911353@qq.com>
2017-10-09 19:54:41 +0800

    requestParam注解

--- spring-guides/quartz-database/src/main/java/cn/tim/quartz/database/utils/ObjectUtils.java
+++ spring-guides/quartz-database/src/main/java/cn/tim/quartz/database/utils/ObjectUtils.java
@@ -15,4 +15,35 @@ public class ObjectUtils {
     public static Map parseToMap(String json) throws IOException {
         return objectMapper.readValue(json, Map.class);
     }
+
+
+    public static void main(String[] args) throws IOException {
+        class Person {
+            private int id;
+
+            private String aName;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getaName() {
+                return aName;
+            }
+
+            public void setaName(String aName) {
+                this.aName = aName;
+            }
+        }
+        Person person = new Person();
+        person.setId(100);
+        person.setaName("aaaa");
+        String json = objectMapper.writeValueAsString(person);
+        Map map = parseToMap(json);
+        System.out.println(map);
+    }
 }
--- spring-guides/submission/src/main/java/cn/tim/submission/controller/GreetingController.java
+++ spring-guides/submission/src/main/java/cn/tim/submission/controller/GreetingController.java
@@ -3,9 +3,10 @@ package cn.tim.submission.controller;
 import cn.tim.submission.entity.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by LuoLiBing on 15/10/26.
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class GreetingController {
 
     @RequestMapping(value = "/greeting", method = RequestMethod.GET)
-    public String greetingForm(Model model) {
-        model.addAttribute("greeting", new Greeting());
+    public String greetingForm(@ModelAttribute(name = "name") String name, @ModelAttribute Greeting greeting) {
+        System.out.println(name);
         return "greeting";
     }
 
@@ -24,5 +25,24 @@ public class GreetingController {
     public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
         model.addAttribute("greeting", greeting);
         return "result";
+    }
+
+    /**
+     * RequestParam和ModelAttribute的区别，后者会在一次请求的前后生效，即如果在request中包含的参数，在响应的时候也可以访问
+     * 而RequestParam仅仅在请求的时候会传递到controller，之后不会在响应中传出。
+     *
+     * RequestParam和没有这个注解的区别，两者最终使用的HandlerMethodArgumentResolver不一样
+     * 没有注解修饰使用的是ServletModelAttributeMethodProcessor这个来处理
+     * 而用@RequestParam修饰的则使用RequestParamMethodArgumentResolver来处理
+     * @param list
+     * @param name
+     * @return
+     */
+    @PutMapping("/long")
+    @ResponseBody
+    public Object acceptLong(List<Long> list, Long name) {
+        System.out.println(list);
+        System.out.println("name = " + name);
+        return Collections.singletonMap("success", "y");
     }
 }
