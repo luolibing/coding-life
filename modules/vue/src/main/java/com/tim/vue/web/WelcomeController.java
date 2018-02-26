2c6d692d744e15d5d7f0a1166fd13429f983736b
luolibing <397911353@qq.com>
2018-02-26 22:17:15 +0800

    Vue

--- modules/vue/src/main/java/com/tim/vue/web/WelcomeController.java
+++ modules/vue/src/main/java/com/tim/vue/web/WelcomeController.java
@@ -2,6 +2,7 @@ package com.tim.vue.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by luolibing on 2018/2/7.
@@ -12,5 +13,10 @@ public class WelcomeController {
     @GetMapping("/welcome")
     public String index() {
         return "welcome";
+    }
+
+    @GetMapping("/wx/{view}")
+    public String view(@PathVariable String view) {
+        return view;
     }
 }
---
+++ modules/vue/src/main/resources/static/js/list.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#app1',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
