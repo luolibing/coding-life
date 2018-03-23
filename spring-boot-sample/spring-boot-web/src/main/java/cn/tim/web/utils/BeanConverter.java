2d6b03ba8e35594fac1a6db950bc835113e3ca8c
luolibing <397911353@qq.com>
2018-03-23 19:40:39 +0800

    beanCopy

---
+++ spring-boot-sample/spring-boot-web/src/main/java/cn/tim/web/utils/BeanConverter.java
@@ -0,0 +1,46 @@
+package cn.tim.web.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+import org.springframework.cglib.beans.BeanCopier;
+
+/**
+ * User: luolibing
+ * Date: 2018/3/23 18:37
+ */
+public class BeanConverter {
+
+    @Test
+    public void convert() throws IllegalAccessException, InstantiationException {
+        Person p = new Person(1L, "luo", "libing");
+        Class<PersonView> viewClazz = PersonView.class;
+        BeanCopier beanCopier = BeanCopier.create(Person.class, viewClazz, false);
+        PersonView personView = viewClazz.newInstance();
+        beanCopier.copy(p, personView, null);
+        System.out.println(personView);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class Person {
+        private Long id;
+
+        private String firstName;
+
+        private String secondName;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class PersonView {
+        private Long id;
+
+        private String firstName;
+
+        private String secondName;
+    }
+}
