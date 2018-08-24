fc452b0ea6b474dc8e0b6126b8bb31b59ad3bcf6
luolibing <397911353@qq.com>
2018-08-24 18:42:14 +0800

    aws swf

---
+++ middleware/aws/aws-swf/src/main/java/cn/tim/aws/swf/workflow/FlowTypes.java
@@ -0,0 +1,11 @@
+package cn.tim.aws.swf.workflow;
+
+public class FlowTypes {
+
+    public static final String DOMAIN = "HelloDomain";
+    public static final String TASKLIST = "HelloTasklist";
+    public static final String WORKFLOW = "HelloWorkflow";
+    public static final String WORKFLOW_VERSION = "1.0";
+    public static final String ACTIVITY = "HelloActivity";
+    public static final String ACTIVITY_VERSION = "1.0";
+}
