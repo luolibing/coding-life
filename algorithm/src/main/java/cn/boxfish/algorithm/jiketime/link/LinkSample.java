c374c0ca6c391f4c5e53f0f9e18dda81cc91ad5a
luolibing <397911353@qq.com>
2019-01-25 17:11:28 +0800

    link

--- algorithm/src/main/java/cn/boxfish/algorithm/jiketime/link/LinkSample.java
+++ algorithm/src/main/java/cn/boxfish/algorithm/jiketime/link/LinkSample.java
@@ -110,24 +110,121 @@ public class LinkSample {
         }
     }
 
-    public static <N> Node<N> mergeSortLinke() {
-        return null;
+    /**
+     * 3 合并已经排序完的两个链表，递归调用
+     * @param leftNode
+     * @param rightNode
+     * @param <N>
+     * @return
+     */
+    public static <N extends Comparable<N>> Node<N> mergeSortLink(Node<N> leftNode, Node<N> rightNode) {
+        // 是否到末尾，leftNode为空，表明是右边的节点
+        if(leftNode == null) {
+            return rightNode;
+        }
+
+        if(rightNode == null) {
+            return leftNode;
+        }
+
+        Node<N> headNode;
+        // left <= right，如果左边的小，则将左边的标往后移
+        if(leftNode.data.compareTo(rightNode.data) < 0) {
+            headNode = leftNode;
+            headNode.nextNode = mergeSortLink(leftNode.nextNode, rightNode);
+        } else {
+            headNode = rightNode;
+            headNode.nextNode = mergeSortLink(leftNode, rightNode.nextNode);
+        }
+
+        return headNode;
     }
+
+    /**
+     * 有3种解法
+     * 一 是直接遍历一次，计算出有多少个元素，然后再次遍历总数-倒数次，这个节点就是要删除的节点，这个要遍历size + index 次
+     * 二 倒数index，遍历的时候，每走1步-1，直到末尾。遍历完之后，如果结果=0，表明表头为倒数index个，
+     *    如果结果>0表明，没有倒数index这个数，小于0的话，则再次遍历，每走1步+1，直到结果等于index，这个节点就是要删除的节点。除最好结果需要遍历size次，其他都需要size + index
+     * 三 用2个指针，一个先走index次，如果正好走到末尾，则表头为倒数index.如果index没走完就到末尾，表明没有倒数index，如果没到末尾，第二个指针再开始走，当第一个到末尾时，则第二个节点的位置为要删除的节点
+     *    总次数最多只需要size次
+     * @param node
+     * @param index
+     * @param <N>
+     */
+    public static <N> Node<N> deleteReverseObj(Node<N> node, int index) {
+        if(node == null || index < 0) {
+            return null;
+        }
+
+        Node<N> fast = node;
+        Node<N> slow = node;
+        int size = 1;
+        // 走index步
+        while (fast != null && size < index) {
+            fast = fast.nextNode;
+            size++;
+        }
+
+        // index还没走完就到头了，表明倒数index不存在
+        if(fast == null) {
+            return null;
+        }
+
+        Node<N> preNode = null;
+        // 再走完
+        while (fast.nextNode != null) {
+            fast = fast.nextNode;
+            preNode = slow;
+            slow = slow.nextNode;
+        }
+
+        // 头节点为倒数index个节点
+        if(preNode == null) {
+            return node.nextNode;
+        }
+
+        preNode.nextNode = slow.nextNode;
+        return node;
+    }
+
+    /**
+     * 求链表的中间结点
+     * @param list
+     * @param <N>
+     * @return
+     */
+    public static <N> Node<N> getMidNode(Node<N> list) {
+        if(list == null) {
+            return null;
+        }
+
+        // 2个指针，一个一次走1步，一个一次走2步
+        Node<N> fast = list;
+        Node<N> slow = list;
+        while (fast.nextNode != null && fast.nextNode.nextNode != null) {
+            slow = slow.nextNode;
+            fast = fast.nextNode.nextNode;
+        }
+        return slow;
+    }
+
 
     public static void main(String[] args) {
         // 1 链表翻转
-        Node<Integer> head = new Node<>(10);
+        Node<Integer> head = new Node<>(0);
         Node<Integer> node = head;
         for(int i = 0; i < 10; i++) {
-            node.nextNode = new Node<>(i);
-            node = node.nextNode;
+            if(i % 2 == 0) {
+                node.nextNode = new Node<>(i);
+                node = node.nextNode;
+            }
         }
 
         Node newHead = reverse(head);
         System.out.println(newHead);
 
         // 2 链表环检测
-        Node<Integer> cycle = new Node<>(10);
+        Node<Integer> cycle = new Node<>(0);
         Node<Integer> cycleNode = null;
         node = cycle;
         for(int i = 0; i < 10; i++) {
@@ -146,5 +243,47 @@ public class LinkSample {
         // 3 链表环
         Node<Integer> enter = getEnter(cycle);
         System.out.println(enter);
+
+
+        Node<Integer> node1 = new Node<>(0);
+        node = node1;
+        for(int i = 0; i < 10; i++) {
+            if(i % 2 == 0) {
+                node.nextNode = new Node<>(i);
+                node = node.nextNode;
+            }
+        }
+
+        Node<Integer> node2 = new Node<>(0);
+        node = node2;
+        for(int i = 0; i < 10; i++) {
+            if(i % 2 == 1) {
+                node.nextNode = new Node<>(i);
+                node = node.nextNode;
+            }
+        }
+        // 4 合并两个有序链表
+        Node<Integer> mergeNode = mergeSortLink(node1, node2);
+        System.out.println(mergeNode);
+
+        // 5 删除链表倒数N个元素
+        Node<Integer> deleteNode = deleteReverseObj(node2, 20);
+        System.out.println(deleteNode);
+        deleteNode = deleteReverseObj(node2, 4);
+        System.out.println(deleteNode);
+        deleteNode = deleteReverseObj(node2, 4);
+        System.out.println(deleteNode);
+
+        // 6 获取链表中间的中点
+        Node<Integer> node3 = new Node<>(0);
+        node = node3;
+        for(int i = 0; i < 10; i++) {
+            if(i % 2 == 1) {
+                node.nextNode = new Node<>(i);
+                node = node.nextNode;
+            }
+        }
+        Node<Integer> midNode = getMidNode(node3);
+        System.out.println(midNode);
     }
 }
