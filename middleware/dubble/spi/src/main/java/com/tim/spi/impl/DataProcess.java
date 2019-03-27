ed96fc8f60feb2aa18fe1440411e939bcd1df143
luolibing <397911353@qq.com>
2019-03-27 08:45:55 +0800

    spi'

--- middleware/dubble/spi/src/main/java/com/tim/spi/impl/DataProcess.java
+++ middleware/dubble/spi/src/main/java/com/tim/spi/impl/DataProcess.java
@@ -4,6 +4,7 @@ package com.tim.spi.impl;
 import com.tim.spi.api.Connector;
 import com.tim.spi.api.Driver;
 import com.tim.spi.api.DriverManager;
+import org.junit.Test;
 
 /**
  * SPI  service provider interface
@@ -12,6 +13,9 @@ import com.tim.spi.api.DriverManager;
  * 通常在api包中定义对应的API规范，不同厂商针对这个规范提供不同的实现，例如java.sql.Driver，mysql和psql分别提供了不同的实现。当引入mysql的包时自动使用mysql的实现，引入psql的包则自动使用
  *
  * 在resources下的META-INF/service创建以接口名称为名的文件，内容即为实现类。
+ *
+ * ServiceLoader的实现，本身返回的是一个Iterator迭代器，内部包含有一个Map，存储对应的接口与实例，当存在的时候直接返回，第一次获取的时候读取配置文件获取并且进行实例化，之后返回结果
+ *
  */
 public class DataProcess {
 
@@ -19,5 +23,17 @@ public class DataProcess {
         Driver driver = DriverManager.getDriver();
         Connector connector = new Connector(driver);
         connector.connect();
+    }
+
+    @Test
+    public void test() {
+        ClassLoader appClassLoader = Thread.currentThread().getContextClassLoader();
+        System.out.println(appClassLoader);
+        System.out.println(appClassLoader.getParent());
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(systemClassLoader);
+        System.out.println(systemClassLoader.getParent());
+        appClassLoader.getResource("META-INF/services/com.tim.spi.api.Driver");
+        systemClassLoader.getResource("META-INF/services/com.tim.spi.api.Driver");
     }
 }
