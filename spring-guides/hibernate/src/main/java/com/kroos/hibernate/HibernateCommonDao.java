8a58f41a019055db252b9896f1766d9f833f2cc3
kroos.luo <kroos.luo@anker.com>
2021-12-22 11:51:04 +0800

    hibernate

--- spring-guides/hibernate/src/main/java/com/kroos/hibernate/HibernateCommonDao.java
+++ spring-guides/hibernate/src/main/java/com/kroos/hibernate/HibernateCommonDao.java
@@ -1,11 +1,15 @@
 package com.kroos.hibernate;
 
+import com.alibaba.ttl.TransmittableThreadLocal;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManagerFactory;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * desc: TODO
@@ -30,5 +34,59 @@ public class HibernateCommonDao extends HibernateDaoSupport implements CommonDao
     public void autowired(EntityManagerFactory entityManagerFactory) {
         SessionFactory sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
         setSessionFactory(sessionFactory);
+    }
+
+    private static final ThreadPoolExecutor executorService = new ThreadPoolExecutor(4, 10, 10L, TimeUnit.SECONDS,
+            new LinkedBlockingDeque<>(1000)) {
+        @Override
+        public void execute(Runnable command) {
+            Runnable myRunnable = new MyRunnable(TENANT_HOLDER.get(), command);
+            super.execute(myRunnable);
+        }
+    };
+
+    private static final ThreadLocal<String> TENANT_HOLDER = new TransmittableThreadLocal<>();
+
+    private static void executeBusiness(String store, Map params) {
+        System.out.println(store);
+        System.out.println(TENANT_HOLDER.get());
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 500; i++) {
+            TENANT_HOLDER.set(i + "");
+            executorService.execute(() -> {
+                try {
+                    System.out.println("新增的日志" + TENANT_HOLDER.get());
+                    executeBusiness("store", Collections.singletonMap("_tenant", "cloud"));
+                    TimeUnit.SECONDS.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        TimeUnit.SECONDS.sleep(Long.MAX_VALUE);
+    }
+
+    public static class MyRunnable implements Runnable {
+
+        private String tenant;
+
+        private Runnable runnable;
+
+        public MyRunnable(String tenant, Runnable runnable) {
+            this.tenant = tenant;
+            this.runnable = runnable;
+        }
+
+        @Override
+        public void run() {
+            TENANT_HOLDER.set(tenant);
+            runnable.run();
+        }
+
+        public String getTenant() {
+            return tenant;
+        }
     }
 }
